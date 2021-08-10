e70d036e3a2f6b95914de2e545d18f8926730728
vieira.vitor42@gmail.com <vieira.vitor42@gmail.com>
2021-08-10 20:10:47 -0300

    Criando a classe Posicao

--- Chess_Console/Program.cs
+++ Chess_Console/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Chess_Console
 {
@@ -10,6 +11,13 @@ namespace Chess_Console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.ReadLine();
         }
     }
 }
